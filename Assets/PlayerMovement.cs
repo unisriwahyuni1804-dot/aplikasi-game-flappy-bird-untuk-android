a62a39c769c1aa8ad2e81815b644a75af3e3d9e8
unisriwahyuni1804-dot <uni.sriwahyuni1804@gmail.com>
2026-01-22 21:34:27 +0800

    Add files via upload

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerMovement : MonoBehaviour
+{
+    // Star is called before the first frame update
+    Rigidbody2D Rb;          // inisialisasi Rigidbody
+    public float jumpForce;  // besar loncatan
+    float score;
+
+    public Text scoreTxt;
+    void Start()
+    {
+        Rb = GetComponent<Rigidbody2D>();  // ambil komponen Rigidbody2D
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreTxt.text = "" + score;
+        if (Input.GetMouseButtonDown(0))  // klik kiri (PC) / tap (HP)
+        {
+            Rb.velocity = Vector2.up * jumpForce;  // naik dengan kecepatan jumpForce
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Poin")
+        {
+            score++;
+            Debug.Log("Score: " + score);
+        }
+
+        if(collision.gameObject.tag =="pipa")
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ Assets/pipeScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pipeScript : MonoBehaviour
+{
+    float nilaiRandom; //nilai desimal
+    // Start is called before the first frame update
+    void Start()
+    {
+        nilaiRandom = Random.Range(0.98f, 4.9f);
+        transform.position = new Vector2(transform.position.x, nilaiRandom);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, Vector2.left * 100, Time.deltaTime * 5);
+    }
+}
